a53bd8873a22995199bddd3c63944d0c836bd592
Dmytro <sharpfuryz@gmail.com>
2022-08-08 13:28:59 +0300

    feat: add imports

--- tron_objects_api.go
+++ tron_objects_api.go
@@ -3,6 +3,9 @@ package tron_objects_api
 import (
 	"fmt"
 	_ "github.com/kattana-io/tron-objects-api/pkg/api"
+	_ "github.com/kattana-io/tron-objects-api/pkg/justomoney"
+	_ "github.com/kattana-io/tron-objects-api/pkg/sunswap"
+	_ "github.com/kattana-io/tron-objects-api/pkg/trc20"
 	_ "github.com/kattana-io/tron-objects-api/pkg/url"
 )
 
