878659d695ec32fd0d88f063e0a3a7b598089045
Dmytro <sharpfuryz@gmail.com>
2022-08-09 09:54:50 +0300

    feat: cover pairs that previously failed

--- pkg/sunswap/Pair_test.go
+++ pkg/sunswap/Pair_test.go
@@ -24,6 +24,38 @@ func TestSunswapPair_GetTokenAddress(t *testing.T) {
 			},
 			want: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
 		},
+		{
+			name: "KBC/TRX",
+			fields: fields{
+				api:     api.NewApi("", nil, url.NewTrongridUrlProvider()),
+				address: *api.FromBase58("TLP9cpp3B8WQNUXgbvjuYvvWUpmA4bzv4V"),
+			},
+			want: "TUGrjLMegH5jvnaS2at6inkmNAWvtqTRFa",
+		},
+		{
+			name: "BAGH/TRX",
+			fields: fields{
+				api:     api.NewApi("", nil, url.NewTrongridUrlProvider()),
+				address: *api.FromBase58("TLYE9Qz3Kue6EV8Na5aNki9Jkk8rZHp8Yo"),
+			},
+			want: "TKZyr8jUu3aZZtUNQ6cRzML91oPUvUKxEJ",
+		},
+		{
+			name: "BC/TRX",
+			fields: fields{
+				api:     api.NewApi("", nil, url.NewTrongridUrlProvider()),
+				address: *api.FromBase58("TLZbTD6Yg6iBWX6wKvYAsxE83vwweRVtuU"),
+			},
+			want: "TJ7s4HjC1dYapZZnMq96VD6XcEEH3GKx1x",
+		},
+		{
+			name: "META/TRX",
+			fields: fields{
+				api:     api.NewApi("", nil, url.NewTrongridUrlProvider()),
+				address: *api.FromBase58("TLaeHRNDoP2YaccQwqzYJdJjwfzJ6DcKR5"),
+			},
+			want: "TPvyF8CD6eknF7hgfoZgZ9capryqQALQ52",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -32,7 +64,7 @@ func TestSunswapPair_GetTokenAddress(t *testing.T) {
 				address: tt.fields.address,
 			}
 			if got, _ := s.GetTokenAddress(); got.ToBase58() != tt.want {
-				t.Errorf("GetTokenAddress() = %v, want %v", got, tt.want)
+				t.Errorf("GetTokenAddress() = %v, want %v", got.ToBase58(), tt.want)
 			}
 		})
 	}
