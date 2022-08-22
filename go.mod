65dbe5653b23cceba286e898b2c8d5883ff69537
Andrii Prokhorovych <prohorovychua@gmail.com>
2022-08-22 12:37:30 +0300

    feat: add re-try function to decimals

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/kattana-io/tron-objects-api
+
+go 1.18
+
+require (
+	github.com/ethereum/go-ethereum v1.10.21
+	github.com/shengdoushi/base58 v1.0.0
+	go.uber.org/zap v1.22.0
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
--- pkg/trc20/Token.go
+++ pkg/trc20/Token.go
@@ -1,6 +1,8 @@
 package trc20
 
-import "github.com/kattana-io/tron-objects-api/pkg/api"
+import (
+	"github.com/kattana-io/tron-objects-api/pkg/api"
+)
 
 type Token struct {
 	api     *api.Api
@@ -11,6 +13,21 @@ func NewTRC20Token(api *api.Api, address *api.Address) *Token {
 	return &Token{
 		api:     api,
 		address: address,
+	}
+}
+
+func (t *Token) TryToGetDecimals(try int64) (int32, bool) {
+	if try > 5 {
+		return 0, false
+	}
+
+	decimals, err := t.api.GetTokenDecimals(t.address.ToHex())
+
+	if err != nil {
+		try += 1
+		return t.TryToGetDecimals(try)
+	} else {
+		return decimals, true
 	}
 }
 
