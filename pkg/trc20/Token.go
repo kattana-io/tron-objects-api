80785333f1e6e1345d01c5744a25b00b369533d2
Andrii Prokhorovych <prohorovychua@gmail.com>
2022-08-22 12:40:20 +0300

    fix: rename NewTRC20 -> New

--- pkg/trc20/Token.go
+++ pkg/trc20/Token.go
@@ -9,7 +9,7 @@ type Token struct {
 	address *api.Address
 }
 
-func NewTRC20Token(api *api.Api, address *api.Address) *Token {
+func New(api *api.Api, address *api.Address) *Token {
 	return &Token{
 		api:     api,
 		address: address,
