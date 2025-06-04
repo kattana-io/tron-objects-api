4dcaf90b8e2d130675ab8d5bf6b5ed3f4aa55be6
commedesvlados <comme.des.vlados@gmail.com>
2025-06-04 13:29:49 +0300

    refactor

--- pkg/types/address.go
+++ pkg/types/address.go
@@ -111,3 +111,14 @@ func s256(s []byte) []byte {
 	bs := h.Sum(nil)
 	return bs
 }
+
+// "000000000000000000000000a614f803b6fd780986a42c78ec9c7f77e6ded13c" -> "a614f803b6fd780986a42c78ec9c7f77e6ded13c"
+func TrimZeroes(address string) string {
+	idx := 0
+	for ; idx < len(address); idx++ {
+		if address[idx] != '0' {
+			break
+		}
+	}
+	return address[idx:]
+}
